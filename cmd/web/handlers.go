02ecb94e5ae2d1828538065ab0c4cdbb71d66489
Syed Shaheer Khalid <shaheerkjaffer@gmail.com>
2026-01-08 17:28:44 +0500

    feat: added helpers.go routes.go

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -8,29 +8,29 @@ import (
 	"text/template"
 )
 
-func home(w http.ResponseWriter, r *http.Request) {
+func (app *application) home(w http.ResponseWriter, r *http.Request) {
 
 	files := []string{"./ui/html/pages/base.html", "./ui/html/pages/home.html", "./ui/html/pages/nav.html"}
 	ts, err := template.ParseFiles(files...)
 
 	if err != nil {
 		log.Print(err.Error())
-		http.Error(w, "Internal Server error", http.StatusInternalServerError)
+		app.serverError(w, r, err)
 		return
 	}
 
 	err = ts.ExecuteTemplate(w, "base", nil)
 
 	if err != nil {
-		log.Print(err.Error())
-		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		app.serverError(w, r, err)
+		return
 
 	}
 
 	w.Header().Add("Server", "Go")
 	//w.Write([]byte("Hello from Snippetbox"))
 }
-func snippetView(w http.ResponseWriter, r *http.Request) {
+func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
 	id, err := strconv.Atoi(r.PathValue("id"))
 	if err != nil || id < 1 {
 		http.NotFound(w, r)
@@ -39,10 +39,10 @@ func snippetView(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Display a specific snippet with ID %d...",
 		id)
 }
-func snippetCreate(w http.ResponseWriter, r *http.Request) {
+func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Display a form for creating a new snippet..."))
 }
-func snippetCreatePost(w http.ResponseWriter, r *http.Request) {
+func (app *application) snippetCreatePost(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusCreated)
 	w.Write([]byte("Save a new snippet..."))
 }
--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,23 +1,57 @@
 package main
 
 import (
-	"log"
+	"database/sql"
+	"flag"
+	"log/slog"
 	"net/http"
+	"os"
+
+	_ "github.com/go-sql-driver/mysql"
 )
+
+type application struct {
+	logger *slog.Logger
+}
 
 func main() {
 
-	fileserver := http.FileServer(http.Dir("./ui/static/"))
+	addr := flag.String("addr", ":4000", "HTTP network address")
 
-	mux := http.NewServeMux()
-	mux.HandleFunc("GET /{$}", home)
-	mux.HandleFunc("GET /snippet/view/{id}", snippetView)
-	mux.HandleFunc("GET /snippet/create", snippetCreate)
-	mux.HandleFunc("POST /snippet/create", snippetCreatePost)
-	mux.Handle("GET /static/", http.StripPrefix("/static", fileserver))
-	log.Print("starting server on :4000")
+	dsn := flag.String("dsn", "shaheer:110434@/snippetbox?parseTime=true", "MySQL DSN String")
 
-	err := http.ListenAndServe(":4000", mux)
-	log.Fatal(err)
+	flag.Parse()
+	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
 
+	db, err := openDB(*dsn)
+	if err != nil {
+		logger.Error(err.Error())
+		os.Exit(1)
+	}
+
+	defer db.Close()
+
+	app := &application{
+		logger: logger,
+	}
+
+	logger.Info("Starting server", "addr", *addr)
+
+	err = http.ListenAndServe(*addr, app.routes())
+	logger.Error(err.Error())
+	os.Exit(1)
+
+}
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("mysql", dsn)
+
+	if err != nil {
+		return nil, err
+	}
+	err = db.Ping()
+	if err != nil {
+		db.Close()
+		return nil, err
+	}
+	return db, nil
 }
